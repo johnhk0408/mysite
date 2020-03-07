eb11e8bcfa8dc3df135bc0aeadf257f2e14c16e3
hayato <h.gt380.k@gmail.com>
2020-03-07 16:07:51 +0900

    ruby_study

---
+++ each.rb
@@ -0,0 +1,19 @@
+#each
+numbers = [1, 2, 3, 4, 5,]
+# numbers.each do |number|
+#   puts number
+# end
+
+# colors = ['red', 'green', 'blue']
+# colors.each do |color|
+#   puts color
+# end
+
+# travel = ['Korea', 'Okinawa', 'Kagawa']
+# travel.each do |travel|
+#   puts travel
+# end
+
+for number in numbers do
+  puts numbers
+end
